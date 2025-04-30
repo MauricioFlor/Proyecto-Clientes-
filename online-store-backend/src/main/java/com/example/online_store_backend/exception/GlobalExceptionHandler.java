a84f4bfcc38827649f5b3635ed652ba54d753ddc
Werner Holters <wholterszabala@gmail.com>
2025-04-30 16:38:46 -0400

    refactor(exception): Organize import statements for clarity and good practices

--- online-store-backend/src/main/java/com/example/online_store_backend/exception/GlobalExceptionHandler.java
+++ online-store-backend/src/main/java/com/example/online_store_backend/exception/GlobalExceptionHandler.java
@@ -4,7 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
