708c7c0d72e48d9e880871c88da6b7cd33c14f55
Werner Holters <wholterszabala@gmail.com>
2025-04-30 16:06:55 -0400

    feat(DTO): Add validation annotations - Se agregó una nueva dependencia para la validación de DTOs

--- online-store-backend/src/main/java/com/example/online_store_backend/dto/CustomerDto.java
+++ online-store-backend/src/main/java/com/example/online_store_backend/dto/CustomerDto.java
@@ -1,5 +1,7 @@
 package com.example.online_store_backend.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +13,13 @@ import lombok.Setter;
 @NoArgsConstructor
 public class CustomerDto {
     private Long id;
+
+    @NotBlank(message = "El nombre no puede estar vacío.")
     private String name;
+    
+    @NotBlank(message = "El correo electrónico no puede estar vacío.")
+    @Email(message = "El correo electrónico debe estar en un formato válido.", regexp = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
     private String email;
+    
     private Long customerTypeId; // ID del tipo de cliente
 }
--- online-store-backend/src/main/java/com/example/online_store_backend/dto/CustomerTypeDto.java
+++ online-store-backend/src/main/java/com/example/online_store_backend/dto/CustomerTypeDto.java
@@ -1,5 +1,6 @@
 package com.example.online_store_backend.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,10 @@ import lombok.Setter;
 @NoArgsConstructor
 public class CustomerTypeDto {
     private Long id;
+    
+    @NotBlank(message = "El nombre del tipo no puede estar vacío.")
     private String customerTypeName;
+    
+    @NotBlank(message = "La descripción no puede estar vacía.")
     private String customerDescription;
 }
